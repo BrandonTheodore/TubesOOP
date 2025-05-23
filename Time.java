4c6b7ece34e75b2df17038c59f9d40206eed7baf
Brandon Theodore Ferrinov <19623223@std.stei.itb.ac.id>
2025-05-23 10:03:01 +0700

    Minor Change
    Adding State to Time.java

--- Time.java
+++ Time.java
@@ -4,11 +4,13 @@ public class Time {
     private final int accelerationFactor;
     private final LocalTime startGameTime;
     private final long startRealTimeNano;
+    private String state; // Siang dan malam
 
     public Time(int time, double accelerationFactor) {
         this.startGameTime = LocalTime.of(time, 0);
         this.accelerationFactor = 300;
         this.startRealTimeNano = System.nanoTime();
+        this.state = "Siang";
     }
 
     public LocalTime getCurrentGameTime() {
@@ -17,6 +19,21 @@ public class Time {
 
         long gameElapsedSeconds = (long) (realElapsedSeconds * accelerationFactor);
         return startGameTime.plusSeconds(gameElapsedSeconds);
+    }
+
+    public String getState() {
+        LocalTime currentTime = getCurrentGameTime();
+
+        LocalTime dayStart = LocalTime.of(06, 00);
+        LocalTime dayEnd = LocalTime.of(17, 59);
+
+        if(currentTime.equals(dayStart) || currentTime.equals(dayEnd) || (currentTime.isAfter(dayStart) && currentTime.isBefore(dayEnd))){
+            this.state = "Siang";
+        } else {
+            this.state = "Malam";
+        }
+
+        return state;
     }
 
     public String getFormattedGameTime() {
