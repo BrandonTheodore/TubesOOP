484ee42ecca369b4022be03f4580a35d46fc5c07
Brandon Theodore Ferrinov <19623223@std.stei.itb.ac.id>
2025-05-24 21:19:23 +0700

    Creating main

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Map iniMap = new Map();
+        iniMap.generateMap();
+        iniMap.printMap();
+    }
+}
