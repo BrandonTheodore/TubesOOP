802f392ee8c74fca0f6a337f4ae30981b6f84f14
princessfaazzahra <19623140@std.stei.itb.ac.id>
2025-05-27 17:37:45 +0700

    fix fish

--- Fish.java
+++ Fish.java
@@ -1,14 +1,16 @@
 import java.util.List;
+import java.time.LocalTime;
+
 
 public class Fish extends Item {
     private List<Season> seasons;
     private List<Weather> weathers;
     private List<LocationFish> locations;
-    private String timeStart;
-    private String timeEnd;
+    private LocalTime timeStart;
+    private LocalTime timeEnd;
     private Rarity rarity;
-    
-    public Fish(String name, int sellPrice, Rarity rarity, List<Season> seasons, List<Weather> weathers, List<LocationFish> locations, String timeStart, String timeEnd) {
+   
+    public Fish(String name, int sellPrice, Rarity rarity, List<Season> seasons, List<Weather> weathers, List<LocationFish> locations, LocalTime timeStart, LocalTime timeEnd) {
         super(name, 0, calculateSellPrice(rarity, seasons.size(), calculateHourRange(timeStart, timeEnd), weathers.size(), locations.size()), ItemCategory.FISH);
         this.rarity = rarity;
         this.seasons = seasons;
@@ -17,59 +19,75 @@ public class Fish extends Item {
         this.timeStart = timeStart;
         this.timeEnd = timeEnd;
     }
-    
+   
     public List<Season> getSeasons() {
         return seasons;
     }
-    
+   
     public List<Weather> getWeathers() {
         return weathers;
     }
-    
+   
     public List<LocationFish> getLocations() {
         return locations;
     }
-    
-    public String getTimeStart() {
+   
+    public LocalTime getTimeStart() {
         return timeStart;
     }
-    
-    public String getTimeEnd() {
+   
+    public LocalTime getTimeEnd() {
         return timeEnd;
     }
-    
+   
     public Rarity getFishRarity() {
         return rarity;
     }
-    
-    public boolean isCatchable(Season currentSeason, Weather currentWeather, LocationFish currentLocation, String currentTime) {
+
+   
+    public boolean isCatchable(Season currentSeason, Weather currentWeather, LocationFish currentLocation, LocalTime currentTime) {
         // season weather location hrs match
         boolean seasonMatch = seasons.contains(currentSeason);
         boolean weatherMatch = weathers.contains(currentWeather);
         boolean locationMatch = locations.contains(currentLocation);
-        
-        // ngubah waktu saat ini ke format jam
-        int currentHour = parseTimeToHour(currentTime);
-        int startHour = parseTimeToHour(timeStart);
-        int endHour = parseTimeToHour(timeEnd);
-        
-        // waktu harus match
-        boolean timeMatch;
-        if (endHour < startHour) { // kalo melewati tengah malam (18:00 - 02:00)
-            timeMatch = (currentHour >= startHour) || (currentHour <= endHour);
-        } else { // kalo normal (06:00 - 18:00)
-            timeMatch = (currentHour >= startHour) && (currentHour <= endHour);
-        }
-        
+       
+        boolean timeMatch = isTimeInRange(currentTime, timeStart, timeEnd);
+       
+       
         return seasonMatch && weatherMatch && locationMatch && timeMatch;
     }
-    
-    // mengubah string waktu menjadi jam
-    private int parseTimeToHour(String time) {
-        String[] parts = time.split(":");
-        return Integer.parseInt(parts[0]);
-    }
 
+
+    private boolean isTimeInRange(LocalTime currentTime, LocalTime startTime, LocalTime endTime) {
+        // buat kasus kalo endTime lebih kecil drpd startTime kek 02.00 dan 20.00
+        if (endTime.isBefore(startTime)) {
+            // currentTime bisa di antara startTime sampai 23:59 ATAU 00:00 sampai endTime
+            return !currentTime.isBefore(startTime) || !currentTime.isAfter(endTime);
+        } else {
+            // Waktu normal dalam satu hari
+            // currentTime harus di antara startTime dan endTime
+            return !currentTime.isBefore(startTime) && !currentTime.isAfter(endTime);
+        }
+    }  
+
+
+    private static int calculateHourRange(LocalTime timeStart, LocalTime timeEnd) {
+        int startHour = timeStart.getHour();
+        int endHour = timeEnd.getHour();
+        
+        int hourRange;
+        
+        if (endHour < startHour) {
+            hourRange = (24 - startHour) + endHour;
+        } 
+        else {
+            hourRange = endHour - startHour;
+        }
+        
+        // Pastikan hourRange minimal 1 untuk menghindari pembagian dengan 0
+        return Math.max(hourRange, 1);
+    }
+   
     public void useItem(Player player, Item item) {
         if (player != null) {
             Inventory inventory = player.getInventory();
@@ -77,25 +95,14 @@ public class Fish extends Item {
                 player.addEnergy(1); // kalo makan ikan energi nambah 1
                 inventory.removeItem(this, 1); // remove item dari inventory
                 System.out.println("Player makan ikan " + getName() + " ,energy +1");
-            } 
+            }
             else {
                 System.out.println(getName() + " is not available in your inventory.");
             }
         }
     }
-    
-    // ngitung rentang jam
-    private static int calculateHourRange(String timeStart, String timeEnd) {
-        int start = Integer.parseInt(timeStart.replace(":", "")) / 100;
-        int end = Integer.parseInt(timeEnd.replace(":", "")) / 100;
-        
-        if (end < start) {
-            return (24 - start) + end;
-        } else {
-            return end - start;
-        }
-    }
-    
+   
+   
     public static int calculateSellPrice(Rarity rarity, int seasonCount, int hourCount, int weatherCount, int locationCount) {
         double baseMultiplier = (4.0 / seasonCount) * (24.0 / hourCount) * (2.0 / weatherCount) * (4.0 / locationCount);
         int rarityMultiplier;
--- FishManager.java
+++ FishManager.java
@@ -1,43 +1,45 @@
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class FishManager {
     private List<Fish> allFish;
-    
+   
     public FishManager() {
         allFish = new ArrayList<>();
-        
+       
         // common fish
         List<Season> anySeasons = Arrays.asList(Season.SUMMER, Season.FALL, Season.WINTER, Season.SPRING);
         List<Weather> anyWeathers = Arrays.asList(Weather.SUNNY, Weather.RAINY);
-        
-        allFish.add(new Fish("Bullhead", 0, Rarity.COMMON, anySeasons, anyWeathers, Arrays.asList(LocationFish.MOUNTAIN_LAKE), "00:00", "23:59"));
-        allFish.add(new Fish("Carp", 0, Rarity.COMMON, anySeasons, anyWeathers, Arrays.asList(LocationFish.MOUNTAIN_LAKE, LocationFish.POND), "00:00", "23:59"));
-        allFish.add(new Fish("Chub", 0, Rarity.COMMON, anySeasons, anyWeathers, Arrays.asList(LocationFish.FOREST_RIVER, LocationFish.MOUNTAIN_LAKE), "00:00", "23:59"));
-        
+       
+        allFish.add(new Fish("Bullhead", 0, Rarity.COMMON, anySeasons, anyWeathers, Arrays.asList(LocationFish.MOUNTAIN_LAKE), LocalTime.of(00,00), LocalTime.of(23,59)));
+        allFish.add(new Fish("Carp", 0, Rarity.COMMON, anySeasons, anyWeathers, Arrays.asList(LocationFish.MOUNTAIN_LAKE, LocationFish.POND), LocalTime.of(00,00), LocalTime.of(23,59)));
+        allFish.add(new Fish("Chub", 0, Rarity.COMMON, anySeasons, anyWeathers, Arrays.asList(LocationFish.FOREST_RIVER, LocationFish.MOUNTAIN_LAKE), LocalTime.of(00,00), LocalTime.of(23,59)));
+       
         // regular fish
-        allFish.add(new Fish("Largemouth Bass", 0, Rarity.REGULAR, anySeasons, anyWeathers, Arrays.asList(LocationFish.MOUNTAIN_LAKE), "06:00", "18:00"));
-        allFish.add(new Fish("Rainbow Trout", 0, Rarity.REGULAR, Arrays.asList(Season.SUMMER), Arrays.asList(Weather.SUNNY), Arrays.asList(LocationFish.FOREST_RIVER, LocationFish.MOUNTAIN_LAKE), "06:00", "18:00"));
-        allFish.add(new Fish("Sturgeon", 0, Rarity.REGULAR, Arrays.asList(Season.SUMMER, Season.WINTER), anyWeathers, Arrays.asList(LocationFish.MOUNTAIN_LAKE), "06:00", "18:00"));
-        allFish.add(new Fish("Midnight Carp", 0, Rarity.REGULAR, Arrays.asList(Season.WINTER, Season.FALL), anyWeathers, Arrays.asList(LocationFish.MOUNTAIN_LAKE, LocationFish.POND), "20:00", "02:00"));
-        allFish.add(new Fish("Flounder", 0, Rarity.REGULAR, Arrays.asList(Season.SPRING, Season.SUMMER), anyWeathers, Arrays.asList(LocationFish.OCEAN), "06:00", "22:00"));
-        allFish.add(new Fish("Halibut", 0, Rarity.REGULAR, anySeasons, anyWeathers, Arrays.asList(LocationFish.OCEAN), "06:00", "11:00"));
-        allFish.add(new Fish("Halibut", 0, Rarity.REGULAR, anySeasons, anyWeathers, Arrays.asList(LocationFish.OCEAN), "19:00", "02:00"));
-        allFish.add(new Fish("Octopus", 0, Rarity.REGULAR, Arrays.asList(Season.SUMMER), anyWeathers, Arrays.asList(LocationFish.OCEAN), "06:00", "22:00"));
-        allFish.add(new Fish("Pufferfish", 0, Rarity.REGULAR, Arrays.asList(Season.SUMMER), Arrays.asList(Weather.SUNNY), Arrays.asList(LocationFish.OCEAN), "00:00", "16:00"));
-        allFish.add(new Fish("Sardine", 0, Rarity.REGULAR, anySeasons, anyWeathers, Arrays.asList(LocationFish.OCEAN), "06:00", "18:00"));
-        allFish.add(new Fish("Super Cucumber", 0, Rarity.REGULAR, Arrays.asList(Season.SUMMER, Season.FALL, Season.WINTER), anyWeathers, Arrays.asList(LocationFish.OCEAN), "18:00", "02:00"));
-        allFish.add(new Fish("Catfish", 0, Rarity.REGULAR, Arrays.asList(Season.SPRING, Season.SUMMER, Season.FALL), Arrays.asList(Weather.RAINY), Arrays.asList(LocationFish.FOREST_RIVER, LocationFish.POND), "06:00", "22:00"));
-        allFish.add(new Fish("Salmon", 0, Rarity.REGULAR, Arrays.asList(Season.FALL), anyWeathers, Arrays.asList(LocationFish.FOREST_RIVER), "06:00", "18:00"));
-        
+        allFish.add(new Fish("Largemouth Bass", 0, Rarity.REGULAR, anySeasons, anyWeathers, Arrays.asList(LocationFish.MOUNTAIN_LAKE), LocalTime.of(06,00), LocalTime.of(18,00)));
+        allFish.add(new Fish("Rainbow Trout", 0, Rarity.REGULAR, Arrays.asList(Season.SUMMER), Arrays.asList(Weather.SUNNY), Arrays.asList(LocationFish.FOREST_RIVER, LocationFish.MOUNTAIN_LAKE), LocalTime.of(06,00), LocalTime.of(18,00)));
+        allFish.add(new Fish("Sturgeon", 0, Rarity.REGULAR, Arrays.asList(Season.SUMMER, Season.WINTER), anyWeathers, Arrays.asList(LocationFish.MOUNTAIN_LAKE), LocalTime.of(06,00), LocalTime.of(18,00)));
+        allFish.add(new Fish("Midnight Carp", 0, Rarity.REGULAR, Arrays.asList(Season.WINTER, Season.FALL), anyWeathers, Arrays.asList(LocationFish.MOUNTAIN_LAKE, LocationFish.POND), LocalTime.of(20,00), LocalTime.of(02,00)));
+        allFish.add(new Fish("Flounder", 0, Rarity.REGULAR, Arrays.asList(Season.SPRING, Season.SUMMER), anyWeathers, Arrays.asList(LocationFish.OCEAN), LocalTime.of(06,00), LocalTime.of(22,00)));
+        allFish.add(new Fish("Halibut", 0, Rarity.REGULAR, anySeasons, anyWeathers, Arrays.asList(LocationFish.OCEAN), LocalTime.of(06,00), LocalTime.of(11,00)));
+        allFish.add(new Fish("Halibut", 0, Rarity.REGULAR, anySeasons, anyWeathers, Arrays.asList(LocationFish.OCEAN), LocalTime.of(19,00), LocalTime.of(02,00)));
+        allFish.add(new Fish("Octopus", 0, Rarity.REGULAR, Arrays.asList(Season.SUMMER), anyWeathers, Arrays.asList(LocationFish.OCEAN), LocalTime.of(06,00), LocalTime.of(22,00)));
+        allFish.add(new Fish("Pufferfish", 0, Rarity.REGULAR, Arrays.asList(Season.SUMMER), Arrays.asList(Weather.SUNNY), Arrays.asList(LocationFish.OCEAN), LocalTime.of(00,00), LocalTime.of(16,00)));
+        allFish.add(new Fish("Sardine", 0, Rarity.REGULAR, anySeasons, anyWeathers, Arrays.asList(LocationFish.OCEAN), LocalTime.of(06,00), LocalTime.of(18,00)));
+        allFish.add(new Fish("Super Cucumber", 0, Rarity.REGULAR, Arrays.asList(Season.SUMMER, Season.FALL, Season.WINTER), anyWeathers, Arrays.asList(LocationFish.OCEAN), LocalTime.of(18,00), LocalTime.of(02,00)));
+        allFish.add(new Fish("Catfish", 0, Rarity.REGULAR, Arrays.asList(Season.SPRING, Season.SUMMER, Season.FALL), Arrays.asList(Weather.RAINY), Arrays.asList(LocationFish.FOREST_RIVER, LocationFish.POND), LocalTime.of(06,00), LocalTime.of(22,00)));
+        allFish.add(new Fish("Salmon", 0, Rarity.REGULAR, Arrays.asList(Season.FALL), anyWeathers, Arrays.asList(LocationFish.FOREST_RIVER), LocalTime.of(06,00), LocalTime.of(18,00)));
+       
         // legendary fish
-        allFish.add(new Fish("Angler", 0, Rarity.LEGENDARY, Arrays.asList(Season.FALL), anyWeathers, Arrays.asList(LocationFish.POND), "08:00", "20:00"));
-        allFish.add(new Fish("Crimsonfish", 0, Rarity.LEGENDARY, Arrays.asList(Season.SUMMER), anyWeathers, Arrays.asList(LocationFish.OCEAN), "08:00", "20:00"));
-        allFish.add(new Fish("Glacierfish", 0, Rarity.LEGENDARY, Arrays.asList(Season.WINTER), anyWeathers, Arrays.asList(LocationFish.FOREST_RIVER), "08:00", "20:00"));
-        allFish.add(new Fish("Legend", 0, Rarity.LEGENDARY, Arrays.asList(Season.SPRING), Arrays.asList(Weather.RAINY), Arrays.asList(LocationFish.MOUNTAIN_LAKE), "08:00", "20:00"));
+        allFish.add(new Fish("Angler", 0, Rarity.LEGENDARY, Arrays.asList(Season.FALL), anyWeathers, Arrays.asList(LocationFish.POND), LocalTime.of(8,00), LocalTime.of(20,00)));
+        allFish.add(new Fish("Crimsonfish", 0, Rarity.LEGENDARY, Arrays.asList(Season.SUMMER), anyWeathers, Arrays.asList(LocationFish.OCEAN), LocalTime.of(8,00), LocalTime.of(20,0)));
+        allFish.add(new Fish("Glacierfish", 0, Rarity.LEGENDARY, Arrays.asList(Season.WINTER), anyWeathers, Arrays.asList(LocationFish.FOREST_RIVER), LocalTime.of(8,00), LocalTime.of(20,00)));
+        allFish.add(new Fish("Legend", 0, Rarity.LEGENDARY, Arrays.asList(Season.SPRING), Arrays.asList(Weather.RAINY), Arrays.asList(LocationFish.MOUNTAIN_LAKE), LocalTime.of(8,00), LocalTime.of(20,00)));
     }
-    
+   
     // return semua nama ikan
     public List<String> getAllFishNames() {
         List<String> fishNames = new ArrayList<>();
@@ -46,7 +48,7 @@ public class FishManager {
         }
         return fishNames;
     }
-    
+   
     // Method untuk mendapatkan string semua nama ikan
     public String getFishNamesAsString() {
         StringBuilder sb = new StringBuilder();
@@ -58,12 +60,12 @@ public class FishManager {
         }
         return sb.toString();
     }
-    
+   
     // return semua ikan
     public List<Fish> getAllFish() {
         return new ArrayList<>(allFish); // return copy dari array allFish
     }
-    
+   
     // get ikan berdasarkan nama
     public Fish getFishByName(String name) {
         for (Fish fish : allFish) {
@@ -73,9 +75,9 @@ public class FishManager {
         }
         return null;
     }
-    
+   
     // Method untuk mendapatkan ikan yang bisa ditangkap di kondisi tertentu
-    public List<Fish> getCatchableFish(Season season, Weather weather, LocationFish location, String time) {
+    public List<Fish> getCatchableFish(Season season, Weather weather, LocationFish location, LocalTime time) {
         List<Fish> catchableFish = new ArrayList<>();
         for (Fish fish : allFish) {
             if (fish.isCatchable(season, weather, location, time)) {
@@ -84,7 +86,7 @@ public class FishManager {
         }
         return catchableFish;
     }
-    
+   
     // Method untuk mendapatkan ikan berdasarkan rarity
     public List<Fish> getFishByRarity(Rarity rarity) {
         List<Fish> fishByRarity = new ArrayList<>();
