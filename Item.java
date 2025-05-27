362612a08bf2ae5cf6e45bf7eaf0d7dc8a12b57c
princessfaazzahra <19623140@std.stei.itb.ac.id>
2025-05-27 21:06:41 +0700

    refactor item

--- Item.java
+++ Item.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.ArrayList;
 public abstract class Item {
     protected String name;
     protected int sellPrice;
@@ -25,6 +27,35 @@ public abstract class Item {
 
     public int getSellPrice(){
         return sellPrice;
+    }
+
+    public List<Item> ItemDijual(){
+        List<Item> itemsDijual = new ArrayList<>();
+        FishManager fishlist = new FishManager();
+        for (Fish fish : fishlist.getAllFish()) {
+            itemsDijual.add(fish);
+        }
+        SeedsManager seedlist = new SeedsManager();
+        for(Seeds seeds : seedlist.getAllSeeds()){
+            itemsDijual.add(seeds);
+        }
+        CropsManager cropslist = new CropsManager();
+        for(Crops crops : cropslist.getAllCrops()){
+            if(crops.buyPrice != 0){
+                itemsDijual.add(crops);
+            }
+        }
+        FoodManager foodlist = new FoodManager();
+        for(Food food : foodlist.getAllFood()){
+            if(food.buyPrice != 0){
+                itemsDijual.add(food);
+            }
+        }
+        MiscManager misclist = new MiscManager();
+        for(Misc misc : misclist.getAllMiscs()){
+            itemsDijual.add(misc);
+        }
+        return itemsDijual;
     }
 
     public abstract void useItem(Player player, Item item);
